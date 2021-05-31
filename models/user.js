46f817acda1ba99bc30c5d7fa3039954a2655147
Steavn Chen <tom9876555@yahoo.com.tw>
2021-05-31 19:12:31 +0800

    feat: user model is ok

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: false
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: Number,
+    required: true
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
+
