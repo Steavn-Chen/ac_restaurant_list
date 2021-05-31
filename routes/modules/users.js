ad4dd4d81121ddb8f64bbb7130f8d3f84a664734
Steavn Chen <tom9876555@yahoo.com.tw>
2021-05-31 20:55:05 +0800

    feat user registrarion feature is ok

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
+
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -9,6 +11,32 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-
+router.post('/register', (req, res) => {
+  //取得註冊表單的參數
+  const { name, email, password, confirmPassword } = req.body
+  //從拿到的參數跟資料庫比對是否己經註冊
+  User.findOne({ email }).then(user => {
+  //如果己經註冊,就返回到註冊頁
+    if (user) {
+      console.log('此帳號己經註冊')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })      
+    } 
+    //如果還還未註冊,就把表單來的資料寫入資料庫
+      else { 
+      return User.create({
+        name,
+        email,
+        password
+      })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+    }
+  })
+})
 
 module.exports = router
